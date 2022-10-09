c6de908d3433fa41e141bcdd6324e087b6e1846b
darlingfirebox <375141898@qq.com>
2022-10-09 23:01:56 +0800

    Mybatis 多表查询

--- src/main/java/com/example/bootvue/controller/UserController.java
+++ src/main/java/com/example/bootvue/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.bootvue.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.bootvue.entity.User;
+import com.example.bootvue.entity.UserOrder;
+import com.example.bootvue.entity.UserOrderNum;
 import com.example.bootvue.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// RestController与Controller的区别在于返回的对象类型不同
+// RestController返回json对象、字符串等等，Controller返回页面
 @RestController
 public class UserController {
 
@@ -21,6 +27,15 @@ public class UserController {
         System.out.println(users);
         return users;
     }
+
+    @GetMapping(value = "/userorder")
+    public List queryorder(){
+        List<UserOrderNum> userOrder = userMapper.findUserAndOrders();
+        System.out.println(userOrder);
+        return userOrder;
+    }
+
+
     @PostMapping(value = "/new-user")
     public String save(User user){
         int i = userMapper.insert(user);
--- src/main/java/com/example/bootvue/mapper/UserMapper.java
+++ src/main/java/com/example/bootvue/mapper/UserMapper.java
@@ -1,9 +1,10 @@
 package com.example.bootvue.mapper;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.bootvue.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import com.example.bootvue.entity.UserOrder;
+import com.example.bootvue.entity.UserOrderNum;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -11,6 +12,20 @@ import java.util.List;
 public interface UserMapper {
     @Select("select * from user")
     public List<User> findUser();
+
+
+    // mybatis实现多表查询
+    //Results代表最终返回的结果集，当数据库字段名与实体类对应的属性名不一致时，可以使用@Results映射来将其对应起来
+    //Result是结果中的每一项，这里有三个，代表想返回三个字段。其中id和username是user表的，order_num是torder表的
+    //property代表实体类中对象的名称，column代表数据库中表的字段名
+    //one与many是一对一与一对多的区别，利用column返回的字段值，调用目标mapper的方法进行查询，返回结果的类型需要与javaType设置相同
+    @Select("select * from user")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "username", column = "username"),
+            @Result(property = "order_num", column = "uid", javaType = List.class,one = @One(select = "com.example.bootvue.mapper.UserOrderMapper.selectByUid"))
+    })
+    public List<UserOrderNum> findUserAndOrders();
 
     @Insert("insert into user values (#{id},#{username},#{password},#{birthday})")
     public int insert(User user);
